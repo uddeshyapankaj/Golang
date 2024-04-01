1626da62d451c359d4228076565cbdfb124f471d
UDDESHYA PANKAJ <Uddeshyapankaj14@gmail.com>
2024-04-01 23:07:36 +0530

    Learned taking input and types of variable

--- investment_calculator.go
+++ investment_calculator.go
@@ -23,11 +23,30 @@ func main() {
 	// fmt.Println(futureValue)
 
 	// shorter syntax
+	const inflationRate = 10.5
 	var investmentAmount float64 = 100000
 	expectedReturnRate := 5.5
-	years := 10.0
+	var years float64
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// Scan - does not work for string with multiple words
+	// var formulaName string = "hello world"
+
+	// fmt.Print("formaula-Name: ")
+	// fmt.Scan(&formulaName)
+
+	// fmt.Println(formulaName)
+
+	//taking input from user
+	fmt.Print("Investement Amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+	fmt.Print("expectedReturnRate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	newinflatedFutureValue := futureValue * math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(newinflatedFutureValue)
 
 }
